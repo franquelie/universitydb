ce022480c9509ec4ce28c9754a53e48660ac6548
Franquelie Masanque <franquelie@gmail.com>
2025-12-04 15:02:11 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>AMA University & Colleges</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" href="favicon.png">
+	<link rel="logo" href="logo.png">
+</head>
+<body class="body-home">
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	  <div class="container-fluid">
+	    <a class="navbar-brand" href="#">
+	    	<img src="logo.png" width="150">
+	    </a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        <li class="nav-item">
+	          <a class="nav-link active" aria-current="page" href="#">Home</a>
+	        </li>
+	          <a class="nav-link" href="#">Programs</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Contact Us</a>
+	        </li>
+	      </ul>
+	      <ul class="navbar-nav me-right mb-2 mb-lg-0">
+	      	<li class="nav-item">
+	          <a class="nav-link" href="login.php">
+	          Login Portal</a>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+	<div class="image-container">
+		<img src="img/background.jpg" class="img-fluid">
+		<div class="overlay-text">
+			<h2>Unlock Your Future with AMAES!</h2>
+			<p>Enroll to nearby campus among 41 branches nationwide, as well as international branches.</p>
+			<button class="custom-button">WHY AMAES ></button>
+			<section id="inquiry">
+			<form>
+        		<h3>Inquiry Form</h3>
+        		  <div class="mb-3">
+				    <label class="form-label">Name</label>
+				    <input type="text" class="form-control">
+				  </div>
+				  <div class="mb-3">
+				    <label for="exampleInputEmail1" class="form-label">Email address</label>
+				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+				  </div>
+				  <div class="mb-3">
+				    <label class="form-label">Year Level</label>
+				    <input type="text" class="form-control">
+				  </div>
+				  <div class="mb-3">
+				    <label class="form-label">City</label>
+				    <input type="text" class="form-control">
+				  </div>
+			 	  <button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+			</section>
+		</div>
+	</div>
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Student Portal</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="body-login">
+	<nav class="navbar navbar-expand-lg custom-navbar-bg">
+	  <div class="container-fluid">
+	    <a class="navbar-brand" href="#"><img src="white-logo.png" width="150"></a>
+	    <div class="logo-text">Pioneer and Largest Network of Colleges in Asia</div>
+	    <div class="title">Student Portal</div>
+	  </div>
+	</nav>
+	<div class="d-flex justify-content-center align-items-center flex-column">
+    	<form class="login">
+    	  <h3>Login</h3>
+		  <div class="mb-3">
+		    <label class="form-label">Student ID</label>
+		    <input type="text" class="form-control">
+		  </div>
+		  
+		  <div class="mb-3">
+		    <label class="form-label">Password</label>
+		    <input type="password" class="form-control">
+		  </div>
+
+		  <button type="submit" class="custom-signin-button">
+		  Sign In</button>
+		</form>
+	</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>	
+</body>
+</html>
